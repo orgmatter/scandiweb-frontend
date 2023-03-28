50164cb5ea3b83cec5567df9c3d068a88e8238d7
orgmatter <orgmatter@gmail.com>
2023-03-28 09:24:42 +0100

    added a proxy script in my src folder

--- src/config/api-endpoints.js
+++ src/config/api-endpoints.js
@@ -3,28 +3,28 @@ export const ApiEndpoints = {
     port: "9000",
     routes: {
         listProducts: {
-            uri: "/api/v1/products",
+            uri: "api/v1/products",
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
             }
         },
         addProduct: {
-            uri: "/api/v1/product",
+            uri: "api/v1/product",
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             }
         },
         deleteProducts: {
-            uri: "/api/v1/products",
+            uri: "api/v1/products",
             method: "DELETE",
             headers: {
                 "Content-Type": "application/json"
             }
         },
         validateProduct: {
-            uri: "/api/v1/product/validate",
+            uri: "api/v1/product/validate",
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
